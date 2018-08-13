73953e6a41c44e0ab0c282a1cde15402ec35dbf2
Oleh Bondaruk <olebon@ciklum.com>
2018-08-13 20:35:15 +0300

    refactoring 6

--- built/main.js
+++ built/main.js
@@ -7,7 +7,7 @@ const ENV_ARGS = argsParser(process.argv.slice(2));
 const host = (ENV_ARGS.env_host) ? ENV_ARGS.env_host : 'localhost';
 const port = (ENV_ARGS.env_port) ? ENV_ARGS.env_port : '5555';
 const seleniumAddress = (ENV_ARGS.protractor_hub) ? ENV_ARGS.protractor_hub : 'localhost:4444';
-launcher_1.init(null, {
+launcher_1.init(path.resolve(__dirname, './config/config.js'), {
     baseUrl: `http://${host}:${port}`,
     seleniumAddress: `http://${seleniumAddress}/wd/hub` || `http://localhost:4444/wd/hub`,
     specs: [path.resolve(__dirname, './specs/*.spec.js')]
--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ const host = (ENV_ARGS.env_host) ? ENV_ARGS.env_host : 'localhost'
 const port = (ENV_ARGS.env_port) ? ENV_ARGS.env_port : '5555'
 const seleniumAddress = (ENV_ARGS.protractor_hub) ? ENV_ARGS.protractor_hub : 'localhost:4444'
 
-init(null, {
+init(path.resolve(__dirname, './config/config.js'), {
   baseUrl: `http://${host}:${port}`,
   seleniumAddress: `http://${seleniumAddress}/wd/hub` || `http://localhost:4444/wd/hub`,
   specs: [path.resolve(__dirname, './specs/*.spec.js')]
