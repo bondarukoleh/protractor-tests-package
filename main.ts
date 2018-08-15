ae0ac4e0ff919cc41cf25d7adced7e30670ae854
Oleh Bondaruk <olebon@ciklum.com>
2018-08-15 12:13:53 +0300

    direct connect added

--- main.ts
+++ main.ts
@@ -4,10 +4,19 @@ import { init } from 'protractor/built/launcher'
 const ENV_ARGS = argsParser(process.argv.slice(2))
 const host = (ENV_ARGS.env_host) ? ENV_ARGS.env_host : 'localhost'
 const port = (ENV_ARGS.env_port) ? ENV_ARGS.env_port : '5555'
-const seleniumAddress = (ENV_ARGS.protractor_hub) ? ENV_ARGS.protractor_hub : 'localhost:4444'
-
-init(path.resolve(__dirname, './config/config.js'), {
+const seleniumAddress = (ENV_ARGS.protractor_hub) && ENV_ARGS.protractor_hub
+const addConfig = {
   baseUrl: `http://${host}:${port}`,
-  seleniumAddress: `http://${seleniumAddress}/wd/hub` || `http://localhost:4444/wd/hub`,
-  specs: [path.resolve(__dirname, './specs/*.spec.js')]
-})
+  specs: [path.resolve(__dirname, './specs/*.spec.js')],
+}
+
+function makeAdditionConf(conf) {
+  if(seleniumAddress) {
+    conf.seleniumAddress = `http://${seleniumAddress}/wd/hub`
+  } else {
+    conf.directConnect = true
+  }
+}
+makeAdditionConf(addConfig)
+
+init(path.resolve(__dirname, './config/config.js'), addConfig)
